240ce385a32850ebbeeffd71f2055abbf9bf4fe7
Brenda Vivian Namirimo <135052091+Mirimo1967@users.noreply.github.com>
2023-09-15 13:23:55 +0300

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -2,12 +2,12 @@
 
 /**
  * _putchar - writes the character c to stdou
- * @c: The character to print
+ * @a: The character to print
  *
  *      Return: On success 1.
  *  On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+int _putchar(char a)
 {
-	return (write(1, &c, 1));
+	return (write(1, &a, 1));
 }
